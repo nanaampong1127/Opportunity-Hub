0ead69304f866d79af12c3c09e96210197b5a88d
Nana Ampong <itzznana63@gmail.com>
2025-02-17 00:31:24 -0600

    backend

---
+++ backend/demo/src/main/java/com/example/opportunity_hub/OpportunityHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.opportunity_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportunityHubApplication.class, args);
+	}
+
+}
